753314d6dc98a7cfbd6d1c8ae331481c216464ba
Orlando Batista <landito82@gmail.com>
2022-05-16 19:08:59 -0400

    Fix Error

--- app/Http/Controllers/API/ZipcodeControlle.php
+++ app/Http/Controllers/API/ZipcodeControlle.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
 class ZipcodeControlle extends Controller
-{
+{    
     /**
      * Display the specified resource.
      *
@@ -18,8 +18,17 @@ class ZipcodeControlle extends Controller
      */
     public function show($zip_code)
     {
-        $zipcode = DB::table('zipcodes')->where('d_codigo','=',$zip_code)->first();
+        $zipcode = Zipcode::where('d_codigo','=',$zip_code)->first();
 
+        foreach ($zipcode->zipcode->sttlements as $sttlement) {
+            $sttlements[] = array(
+                'key' => $sttlement->id_asenta_cpcons,
+                'name' => Str::upper(Str::ascii($sttlement->d_asenta)),
+                'zone_type' => Str::upper(Str::ascii($sttlement->d_zona)),
+                'settlement_type' => array('name' => $sttlement->d_tipo_asenta)
+            );
+        }
+        
         return response() -> json([
             'zip_code' => (string)sprintf("%05d", $zipcode->d_codigo),
             'locality' => Str::upper(Str::ascii($zipcode->d_ciudad)),
@@ -28,14 +37,7 @@ class ZipcodeControlle extends Controller
                 'name' => Str::upper(Str::ascii($zipcode->d_estado)),
                 'code' => $zipcode->c_CP
             ],
-            'settlements' => [[
-                'key' => $zipcode->id_asenta_cpcons,
-                'name' => Str::upper(Str::ascii($zipcode->d_asenta)),
-                'zone_type' => Str::upper(Str::ascii($zipcode->d_zona)),
-                'settlement_type' => [
-                    'name' => Str::ascii($zipcode->d_tipo_asenta)
-                ]
-            ]],
+            'settlements' => $sttlements,
             'municipality' => [
                 'key' => $zipcode->c_mnpio,
                 'name' => Str::upper(Str::ascii($zipcode->D_mnpio))
--- app/Models/Zipcode.php
+++ app/Models/Zipcode.php
@@ -8,23 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Zipcode extends Model
 {
     use HasFactory;
+ 
+    public function sttlements() {
+        return $this->hasMany(Zipcode::class,'d_codigo');
+    }
 
-    protected $fillable = [
-        'd_codigo', 
-        'd_asenta', 
-        'd_tipo_asenta', 
-        'D_mnpio', 
-        'd_estado', 
-        'd_ciudad', 
-        'd_CP', 
-        'c_estado', 
-        'c_oficina', 
-        'c_CP', 
-        'c_tipo_asenta', 
-        'c_mnpio', 
-        'id_asenta_cpcons', 
-        'd_zona', 
-        'c_cve_ciudad'
-    ];
+    public function zipcode() {
+        return $this->belongsTo(Zipcode::class,'d_codigo');
+    }
 
 }
